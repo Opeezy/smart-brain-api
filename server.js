5b9eef635eebc1b39127df7dd487ccac44778238
Opeezy <copenshaw17@gmail.com>
2018-04-15 15:59:39 -0500

    moved api call into server code

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({
 	    connectionString : process.env.DATABASE_URL,
 	    ssl: true
 	}
-});
+});d
 
 const app = express();
 
@@ -27,6 +27,7 @@ app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 const PORT = process.env.PORT;
 app.listen(PORT || 3000, () => {
