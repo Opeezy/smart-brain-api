7f72aa04811030f10a96925c633fd593c8207153
Opeezy <copenshaw17@gmail.com>
2018-04-15 15:02:34 -0500

    adding db connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({	
 	  client: 'pg',
 	  connection: {
-	    host : 'postgresql-aerodynamic-72362',
-	    user : 'postgres',
-	    password : '',
-	    database : 'smartbrain'
+	    host : process.env.DATABASE_URL,
+	    ssl: true;
 	}
 });
 
